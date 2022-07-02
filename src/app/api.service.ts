c0d657eb236a767b2cf27b741cd67a004868dfab
IanoSang <ianosang18@gmail.com>
2022-07-03 00:47:56 +0300

    feat: service ts- changes the base url to communicate with a deployed backend

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ApiService {
-  baseurl=  "http://127.0.0.1:8000";
+  baseurl=  "https://crudiano.herokuapp.com";
   httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   constructor(private http:HttpClient) { }
   getAllMovies(): Observable<any>{
